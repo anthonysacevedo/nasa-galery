059939f1e22ca7bdcab75cd92900d981a08a22ab
anthonysacevedo <141374766+anthonysacevedo@users.noreply.github.com>
2024-12-19 10:05:38 -0300

    Actualizar init.js

--- init.js
+++ init.js
@@ -31,7 +31,7 @@ const swiper = new Swiper('.swiper', {
 function imprimirContenido(array) {
     const fragment = document.createDocumentFragment();
 
-    for (let i = 6; i < array.length; 15) {
+    for (let i = 6; i < array.length; i++) {
         let item = array[i];
         let imageUrl = item.links?.[0]?.href;
     
